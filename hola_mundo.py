f761c8bfbef50e91b7073946ad646e544567853c
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-09-29 10:03:19 +0200

    Comentarios, entrada, salida de datos y tipos de datos en python

---
+++ hola_mundo.py
@@ -0,0 +1,22 @@
+#Tipos de datos y varaible
+
+#Numéricos
+#enteros -- int
+#real -- float
+#cadena de caracteres -- str
+#Lógico -- bool
+
+nombre = "Ángela"
+edad = 17
+mayor0menorEdad=False
+
+#Entrada y salida de datos
+#Salida con print()
+
+print("Buenos días" , nombre , "y tú edad es:" , edad)
+
+#Entrada de datos -- input()
+nombre = input("Dime tu nombre: \n")   #El\n sirve para dar un espacio
+edad = input("¿Cúántos años tienes?\n")
+
+print("Buenos días" , nombre , "y tú edad es:" , edad)
