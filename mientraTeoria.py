a65f5ad596a20976211e917dedf21095e8585be9
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-10-11 14:40:19 +0200

    teoria del MIENTRAS

---
+++ mientraTeoria.py
@@ -0,0 +1,30 @@
+#bucle
+'''i=0
+bandera=True
+while (bandera == True):
+    print("No hagas esto nunca",i)
+    i = i + 1
+    if (i==30000):
+        bandera=False
+
+i=0
+num=0
+bandera=True
+num=(int)(input("Dime un número: "))
+while (i<=):
+    print(num, "*" ,i, "=",num*i)
+    i=i+1
+'''
+#palabara secreta
+
+palabrasecreta="croquetas"
+palabra=""
+
+palabra=input("Dime una palabra ")
+
+if(palabrasecreta==palabra):
+while (palabrasecreta==palabra):
+    print("Contraseña correcta. Intentalo de nuevo")
+    palabrasecreta=input("Dime una palabra ")
+    else
+    print("contraseña correcta"):
