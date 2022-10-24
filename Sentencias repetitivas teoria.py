c72bd0d2e1757f5389cd75d2b1986d0f0732905b
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-10-24 10:41:08 +0200

    Teoria sentencias repetitivas FOR

---
+++ Sentencias repetitivas teoria.py
@@ -0,0 +1,13 @@
+#while
+
+contador=0
+while contador<=10:
+    print(contador)
+    contador +=1       # contador=contador+1
+
+#BUCLE FOR
+#lista= [1,2,3,4,5,6,7,8,9,10]
+
+vNombres = ["Angel", "Maria", "Pepe", "Jose"]
+for num in vNombres:                            #range(1,100)---> cuando quiere imprimir por pantalla todos los numeros
+    print(num)
