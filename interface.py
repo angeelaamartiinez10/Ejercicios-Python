090207874dff0fabdbf0c8610fc0a550fceb51a5
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-11-18 11:13:39 +0100

    ventana1

--- interface.py
+++ interface.py
@@ -1,9 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+#generar la ventana
+ventana = Tk()
+ventana.title("Primer ejercicio")
+ventana.geometry("400x500")
+ventana.resizable(width=False,height=False)
+ventana.config(background="light blue")
+ventana.iconbitmap()
+
+#genera el lienzo para pintar componentes
+frm = ttk.Frame(ventana).pack()  #paddinf->> anchura de la ventana
+
+
+#componentes label y button
+labelTexto=ttk.Label(frm, text="Hola Tkinter!")
+labelTexto.config(background="sky blue", border=5, font=("arial",15))  #label--->> texto
+labelTexto.pack()
+
+ttk.Button(frm, text="Salir", command=ventana.destroy).pack()      # command-->> nombrar. destroy-->> cierra la ventana
+
+ventana.mainloop()  # bucle que hace que se presente en la vantana
