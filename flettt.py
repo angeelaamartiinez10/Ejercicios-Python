50014386222ab39f2d61713bc2a508d8e1737223
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-12-05 10:46:16 +0100

    TextField:)

--- flettt.py
+++ flettt.py
@@ -5,21 +5,21 @@ import flet as ft
 def main(page: ft.Page):
     page.title="Tkinter no mola nada-->> Flet:))))"
 
-    def cambiarColor(e):
-        t.color="pink"
-        for i in range(10):
-            text=ft.Text(value=f"texto numero {i}",size=30)
-            page.add(text)
-
+    def mostraNombre(e):
+        t.value=textField_Nombre.value
+        page.update()
 
     #componente texto
     t =ft.Text(value="Introducción a flet", color="Cyan",size=25)
     page.add(t) #add hace dos cosas. 1-AÑADIR 2-ACTUALIZAR
 
     #componentes boton
-    bt=ft.FloatingActionButton(icon=ft.icons.ADD, on_click=cambiarColor)   
+    bt=ft.FloatingActionButton(icon=ft.icons.ADD, on_click=mostraNombre)   
     page.add(bt)
 
+    textField_Nombre=ft.TextField(label="Nombre", hint_text="Escribe tu nombre")
+    textField_Nombre.focus()
+    page.add(textField_Nombre)
 
 
 ft.app(target=main)
