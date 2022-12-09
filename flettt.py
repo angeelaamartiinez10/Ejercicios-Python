defcfaf8723f66bfb668c09860198ab3ee8930ea
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-12-09 10:51:46 +0100

    dropdown y slider

--- flettt.py
+++ flettt.py
@@ -10,7 +10,7 @@ def main(page: ft.Page):
         page.update()
 
     #componente texto
-    t =ft.Text(value="Introducción a flet", color="Cyan",size=25)
+    t =ft.Text(value="Introducción a flet", color="blue",size=25)
     page.add(t) #add hace dos cosas. 1-AÑADIR 2-ACTUALIZAR
 
     #componentes boton
@@ -18,8 +18,15 @@ def main(page: ft.Page):
     page.add(bt)
 
     textField_Nombre=ft.TextField(label="Nombre", hint_text="Escribe tu nombre")
-    textField_Nombre.focus()
     page.add(textField_Nombre)
+    
+    dropdown_menu=ft.Dropdown(width=200, options=[ft.dropdown.Option("Mujer"),ft.dropdown.Option("Hombre"),])
+    page.add(dropdown_menu)
+    dropdown_menu.options.append(ft.dropdown.Option("Mejor ni te lo digo"))
+    page.update()
 
+    value=int
+    slider_edad=ft.Slider (min=0, max=120, divisions=120, label="Edad:{value}")
+    page.add(slider_edad)
 
 ft.app(target=main)
