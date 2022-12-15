e1b6ac5a86bd5f1bdf83183d10c4c7e2f0b46589
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-12-15 10:15:29 +0100

    seleccionar

--- listaDeLaCompra.py
+++ listaDeLaCompra.py
@@ -4,18 +4,47 @@ import flet as ft
 def main(page: ft.Page):
     page.title="***Lista de la compra***"
    
-    def boton_seleccionar(e):
-        t.value = (f"Comida seleccionada  {c1.value}, {c2.value}, {c3.value}, {c4.value}, {c5.value}.")
+    def boton_seleccionarC(e):
+        texto.value = (f"Carne seleccionada  {c1.value}, {c2.value}, {c3.value}")
+        page.update()
+
+    def seleccionar_Carne(e):
+        
+        if dropdown_menu.value == "Carne":
+            page.add(c1, c2, c3, b, texto)
+        else:
+            page.remove(c1, c2, c3, b, texto)
+
+    def boton_seleccionarP(e):
+        t.value = (f"Pescado seleccionada  {c1.value}, {c2.value}, {c3.value}")
         page.update()
 
     texto = ft.Text(value="Selecciona")
-    c1 = ft.Checkbox(label="Pollo", value=True)
-    c2 = ft.Checkbox(label="Ternera", value=True)
-    c3 = ft.Checkbox(label="Cerdo", value=True)
-    c4 = ft.Checkbox(label="Chorizo", disabled=True)
-    b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionar)
-    page.add(c1, c2, c3, c4, b, texto)
+    c1 = ft.Checkbox(label="Pollo", value=False)
+    c2 = ft.Checkbox(label="Ternera", value=False)
+    c3 = ft.Checkbox(label="Cerdo", value=False)
+    b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarC)
+    #page.add(c1, c2, c3, b, texto)
 
+    
+
+    texto = ft.Text(value="Selecciona")
+    c1 = ft.Checkbox(label="Sardinas", value=False)
+    c2 = ft.Checkbox(label="Merluza", value=False)
+    c3 = ft.Checkbox(label="Bacalao", value=False)
+    b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarP)
+    #page.add(c1, c2, c3, b, texto)
+
+    def boton_seleccionarV(e):
+        t.value = (f"Verdura seleccionada  {c1.value}, {c2.value}, {c3.value},")
+        page.update()
+
+    texto = ft.Text(value="Selecciona")
+    c1 = ft.Checkbox(label="Tomates", value=False)
+    c2 = ft.Checkbox(label="Lechuga", value=False)
+    c3 = ft.Checkbox(label="Zanahorias", value=False)
+    b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarV)
+    #page.add(c1, c2, c3, b, texto)
 
     def mostraNombre(e):
         t.value=textField_Nombre.value
@@ -35,9 +64,11 @@ def main(page: ft.Page):
     slider_coste=ft.Slider (min=0, max=1000, divisions=1000, label="Coste máximo:{value}€")
     page.add(slider_coste)
 
-    dropdown_menu=ft.Dropdown(width=200, options=[ft.dropdown.Option("Carne"),ft.dropdown.Option("Pescado"),])
+    dropdown_menu=ft.Dropdown(width=200, options=[ft.dropdown.Option("Carne") ,ft.dropdown.Option("Pescado"),ft.dropdown.Option("Verduras")], on_change=seleccionar_Carne)
+  
+   
+
     page.add(dropdown_menu)
-    dropdown_menu.options.append(ft.dropdown.Option("Verduras"))
     page.update()
 
 
