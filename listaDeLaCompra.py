e18451f37a835056c1ce81a2533e0c5c8b2dbdad
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-12-16 11:15:33 +0100

    modificado

--- listaDeLaCompra.py
+++ listaDeLaCompra.py
@@ -5,26 +5,56 @@ def main(page: ft.Page):
     page.title="***Lista de la compra***"
    
     def boton_seleccionarC(e):
-        texto.value = (f"Carne seleccionada  {c1.value}, {c2.value}, {c3.value}")
+        texto.value = (f"Carne seleccionada  {c1c.value}, {c2c.value}, {c3c.value}")
         page.update()
 
     def seleccionar_Carne(e):
-        
-        if dropdown_menu.value == "Carne":
-            page.add(c1, c2, c3, b, texto)
-        else:
+        try:
+            page.remove(c1c, c2c, c3c, bc, textoc)
+        except:
+            print("fallado pero pasamos")
+        try:
             page.remove(c1, c2, c3, b, texto)
+        except:
+            print("fallado pero pasamos")
+        try:
+            page.remove(c1v, c2v, c3v, bv, textov)
+        except:
+            print("fallado pero pasamos")
 
+        if dropdown_menu.value == "Carne":
+            page.add(c1c, c2c, c3c, bc, textoc)
+        elif dropdown_menu.value == "Pescado":
+            page.add(c1, c2, c3, b, texto)
+        elif dropdown_menu.value == "Verduras":
+            page.add(c1v, c2v, c3v, bv, textov)
+        
+
+        
     def boton_seleccionarP(e):
         t.value = (f"Pescado seleccionada  {c1.value}, {c2.value}, {c3.value}")
         page.update()
 
-    texto = ft.Text(value="Selecciona")
-    c1 = ft.Checkbox(label="Pollo", value=False)
-    c2 = ft.Checkbox(label="Ternera", value=False)
-    c3 = ft.Checkbox(label="Cerdo", value=False)
-    b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarC)
-    #page.add(c1, c2, c3, b, texto)
+    def seleccionar_Pescado(e):
+        if dropdown_menu == "Pescado":
+            page.add(c1, c2, c3, b, texto)
+        else:
+            page.remove(c1, c2, c3, b, texto)
+
+    
+
+
+    def boton_seleccionarV(e):
+        t.value = (f"Verdura seleccionada  {c1v.value}, {c2v.value}, {c3v.value},")
+        page.update()
+
+
+    textoc = ft.Text(value="Selecciona")
+    c1c = ft.Checkbox(label="Pollo", value=False)
+    c2c= ft.Checkbox(label="Ternera", value=False)
+    c3c = ft.Checkbox(label="Cerdo", value=False)
+    bc = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarC)
+    #page.add(c1c, c2c, c3c, bc, textoc)
 
     
 
@@ -35,18 +65,15 @@ def main(page: ft.Page):
     b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarP)
     #page.add(c1, c2, c3, b, texto)
 
-    def boton_seleccionarV(e):
-        t.value = (f"Verdura seleccionada  {c1.value}, {c2.value}, {c3.value},")
-        page.update()
 
-    texto = ft.Text(value="Selecciona")
-    c1 = ft.Checkbox(label="Tomates", value=False)
-    c2 = ft.Checkbox(label="Lechuga", value=False)
-    c3 = ft.Checkbox(label="Zanahorias", value=False)
-    b = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarV)
-    #page.add(c1, c2, c3, b, texto)
+    textov = ft.Text(value="Selecciona")
+    c1v = ft.Checkbox(label="Tomates", value=False)
+    c2v = ft.Checkbox(label="Lechuga", value=False)
+    c3v = ft.Checkbox(label="Zanahorias", value=False)
+    bv = ft.ElevatedButton(text="guardar", on_click=boton_seleccionarV)
+    #page.add(c1v, c2v, c3v, bv, textov)
 
-    def mostraNombre(e):
+    def mostraNombre(e):   
         t.value=textField_Nombre.value
         page.update()
         
